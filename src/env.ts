687efcbfbd2c5d8f6c63bb7253428eaf2748f841
Evandro Wendt <48156193+evandrowendt@users.noreply.github.com>
2025-07-08 19:41:00 -0300

    Criacão do servidor HTTP e inicio do DB

---
+++ src/env.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string().url().startsWith('postgresql://'),
+});
+
+export const env = envSchema.parse(process.env);
+
+/*Scrip para fazer valição e parse das variaveis ambiente, para garantir que a aplicação não
+execute caso falte alguma variavel de ambiente obrigatória, nesse caso é setado um valor default para caso a variavel não exista*/
